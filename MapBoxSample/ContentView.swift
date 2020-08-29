b2525567398d289079b7f84b91d364a8cb7d2294
ha1f <half.ldk.hiz@gmail.com>
2020-08-29 18:16:42 +0900

    Added annotations

--- MapBoxSample/ContentView.swift
+++ MapBoxSample/ContentView.swift
@@ -7,14 +7,29 @@
 //
 
 import SwiftUI
+import Mapbox
 
+private extension MGLPointAnnotation {
+    convenience init(title: String, coordinate: CLLocationCoordinate2D) {
+        self.init()
+        self.title = title
+        self.coordinate = coordinate
+    }
+}
+
+/// annotationと中心座標が近すぎると何故か触ったときにクラッシュするので、適当に離してある
 struct ContentView: View {
+    @State var annotations: [MGLPointAnnotation] = [
+        MGLPointAnnotation(title: "Mazzone", coordinate: .init(latitude: 33.594115, longitude: 130.418308))
+    ]
+
     var body: some View {
         NavigationView {
-            MapView()
-                .centerCoordinate(.init(latitude: 37.791293, longitude: -122.396324))
+            MapView(annotations: $annotations)
+                .centerCoordinate(.init(latitude: 33.590507, longitude: 130.430142))
                 .zoomLevel(16)
                 .navigationBarTitle("Map")
+                .edgesIgnoringSafeArea(.all)
         }
     }
 }
--- MapBoxSample/MapView.swift
+++ MapBoxSample/MapView.swift
@@ -10,18 +10,23 @@ import SwiftUI
 import Mapbox
 
 struct MapView: UIViewRepresentable {
-    private let mapView: MGLMapView
-
-    init(styleURL: URL? = MGLStyle.streetsStyleURL) {
-        mapView = MGLMapView(frame: .zero, styleURL: styleURL)
-    }
+    @Binding var annotations: [MGLPointAnnotation]
+    private let mapView: MGLMapView = MGLMapView(frame: .zero, styleURL: MGLStyle.streetsStyleURL)
 
     func makeUIView(context: UIViewRepresentableContext<MapView>) -> MGLMapView {
+        mapView.delegate = context.coordinator
         return mapView
     }
 
     func updateUIView(_ uiView: MGLMapView, context: UIViewRepresentableContext<MapView>) {
-        //
+        updateAnnotations()
+    }
+
+    private func updateAnnotations() {
+        if let currentAnnotations = mapView.annotations {
+            mapView.removeAnnotations(currentAnnotations)
+        }
+        mapView.addAnnotations(annotations)
     }
 
     func centerCoordinate(_ centerCoordinate: CLLocationCoordinate2D) -> MapView {
@@ -32,5 +37,27 @@ struct MapView: UIViewRepresentable {
     func zoomLevel(_ zoomLevel: Double) -> MapView {
         mapView.zoomLevel = zoomLevel
         return self
+    }
+}
+
+extension MapView {
+    final class Coordinator: NSObject, MGLMapViewDelegate {
+        var control: MapView
+
+        init(_ control: MapView) {
+            self.control = control
+        }
+
+        func mapView(_ mapView: MGLMapView, viewFor annotation: MGLAnnotation) -> MGLAnnotationView? {
+            return nil
+        }
+
+        func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
+            return true
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
     }
 }
